87533785233d11d59d533dbfd45f36d1d21dcbb6
Julian Klein <jg.klein@protonmail.com>
2019-12-06 11:46:17 +0100

    Figures script updated to including BACI graphs

---
+++ scripts/bird_figures.R
@@ -0,0 +1,132 @@
+## Make figures for the experimental data and the bpo model responses
+##
+## First edit: 20191028
+## Last edit:  20191204
+##
+## Author: Julian Klein
+
+## 1. Clear environment and load libraries -------------------------------------
+
+rm(list = ls())
+
+require("ggplot2")
+require("rjags")
+require("data.table")
+
+## 2. Define or source functions used in this script ---------------------------
+
+
+## 3. Load and explore data ----------------------------------------------------
+
+forest <- as.data.table(read.csv("clean/forest_experiment_data.csv"))
+head(forest)
+
+BACI_sl <- read.csv("clean/BACI_sl.csv")
+head(BACI_sl)
+
+## 4. Make graphs with the explanatory data ------------------------------------
+
+## Graph with tree numbers:
+
+colnames(forest)
+
+G1data <- melt(forest[,c(2:8,14,16)]) ## tree_nr
+# G1data <- melt(forest[,c(3,9:13,16)]) ## dbh
+# G1data <- melt(forest[,c(3,15,16)]) ## laser
+# G1data <- melt(forest[,c(3,17:20,16)]) ## perc
+
+G1data <- G1data[#forest$experiment != "difference" & 
+                 G1data$treatment %in% c("T","URT"),]
+
+G1summary <- G1data[, list("mean"=mean(value, na.rm=T), 
+                           "sd"=sd(value, na.rm=T)),
+                    by=c("experiment","treatment","variable")]
+
+G1summary$experiment <- factor(G1summary$experiment, 
+                               levels=c("before","after","difference"))
+
+G1 <- ggplot(G1summary, aes(x=treatment, y=mean, fill=experiment)) +
+      geom_bar(stat="identity", position=position_dodge()) +
+      geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
+                    position=position_dodge(.9)) +
+      facet_wrap(. ~ variable, scales="free_y") + 
+      theme_bw(20) +
+      theme(legend.position = "top", 
+            legend.direction = "horizontal",
+            legend.title = element_blank())
+
+png("figures/perc_tree.png", 10000/4, 7000/4, "px", res = 600/4)
+
+G1
+
+dev.off()
+
+## 5. Make graphs for bpo predictions ------------------------------------------
+
+## Graph for slopes with CIs:
+
+## Order:
+BACI_sl$species <- factor(BACI_sl$species, 
+                          levels = unique(BACI_sl$species[order(BACI_sl$X50.)]))
+
+## Community response:
+BACI_sl$cm <- ifelse(BACI_sl$species == "cm", "community mean", "individual")
+
+## Add significance grouping:
+BACI_sl$cross <- ifelse(sign(BACI_sl$X2.5.) == sign(BACI_sl$X97.5.),
+                        "95% CI does not cross zero", 
+                        "95% CI crosses zero")
+
+g1 <- ggplot(data = BACI_sl, 
+             aes(x = species, y = X50., 
+                 colour = treatment, fill = treatment,linetype = cross))
+g2 <- geom_errorbar(aes(ymin = X2.5., ymax = X97.5.), size = 3, width = 0, position = position_dodge(0.5))
+#g3 <- geom_point(position = position_dodge(1), size = 1, color = "black")
+g4 <- facet_grid(cm ~ indicator, space = "free", scales = "free")
+G <- g1 +
+  geom_hline(yintercept = 0, linetype = "dashed") + 
+  g2 + g4 + #g3 +
+  xlab("") + ylab("") +
+  theme_bw(40) + coord_flip() +
+  theme(legend.position = "top", #c(0.7, 0.1),
+        legend.title = element_blank(),
+        legend.key.size = unit(3, 'lines'),
+        legend.direction = "horizontal",
+        strip.text.y = element_blank())
+
+png("figures/BACI_slopes.png", 20000/8, 20000/8, "px", res = 600/8); G; dev.off()
+
+## Graph for probabilies:
+
+## Order:
+BACI_sl$species <- factor(BACI_sl$species, 
+                          levels = unique(BACI_sl$species[order(BACI_sl$ecdf)]))
+
+p1 <- ggplot(data = BACI_sl, aes(x = species, y = 0, colour = treatment))
+p2a <- geom_errorbar(aes(ymin = 0, ymax = BACI_sl$ecdf), 
+                     position = position_dodge(0.5),
+                     size = 3, 
+                     width = 0)
+p2b <- geom_errorbar(aes(ymin = BACI_sl$ecdf - 1, ymax = 0), 
+                     position = position_dodge(0.5),
+                     size = 3,                  
+                     width = 0)
+P <- p1 + 
+  scale_y_continuous(breaks = c(-0.95, -0.5, 0, 0.5, 0.95),
+                     labels = c(".95", ".5", "0", "", ""),
+                     sec.axis = dup_axis(
+                       name = "Probability that the indicator is positive",
+                       labels = c("", "", "0", ".5", ".95"))) +
+  p2a + p2b + g4 +
+  xlab("") + ylab("Probability that the indicator is negative") + 
+  coord_flip() +
+  theme_bw(40) +
+  theme(legend.position = "top", 
+        legend.title = element_blank(),
+        legend.key.size = unit(3, 'lines'),
+        legend.direction = "horizontal",
+        strip.text.y = element_blank())
+
+png("figures/BACI_probs.png", 20000/8, 20000/8, "px", res = 600/8); P; dev.off()
+
+## -------------------------------END-------------------------------------------
